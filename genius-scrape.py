46aaa8c3695565922926b3303fe0b5ba48346b97
Jeff Brennan <jeffbrennan10@gmail.com>
2018-08-21 01:49:29 -0500

    fixed dates, improved speed

--- genius-scrape.py
+++ genius-scrape.py
@@ -1,23 +1,25 @@
 import requests, csv, time, timing, json
 from bs4 import BeautifulSoup
 
-releasedList, linkList, songList, viewList, lyricsList, artistList, titleList, rankList, dateList = ([] for i in range(9))
+releasedList, linkList, songList, viewList, lyricsList, artistList, titleList, dateList = ([] for i in range(8))
 page = [1,2]
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', \
+        'August', 'September', 'October', 'November', 'December']
+outputPath = 'C:/Users/jeffb/Documents/Python/webPrograms/webScraping/genius/output/genius/'
+fileDate = time.strftime('%Y-%m-%d')
 
 checked = time.time()
 
 def getInfo (page):
     r = requests.get('https://genius.com/api/songs/chart?page='+str(page)+'&per_page=50&time_period=day')
     data = json.loads(r.content.decode())
-    x = 0 
 
-    while x < 50:
-        link = data['response']['chart_items'][x]['item']['path']
-        songID = data['response']['chart_items'][x]['item']['id']
-        views = data['response']['chart_items'][x]['item']['stats']['pageviews']
-        artist = data['response']['chart_items'][x]['item']['primary_artist']['name']       
-        title = data['response']['chart_items'][x]['item']['title']
+    for i in range(0, 50):
+        link = data['response']['chart_items'][i]['item']['path']
+        songID = data['response']['chart_items'][i]['item']['id']
+        views = data['response']['chart_items'][i]['item']['stats']['pageviews']
+        artist = data['response']['chart_items'][i]['item']['primary_artist']['name']       
+        title = data['response']['chart_items'][i]['item']['title']
 
         linkList.append(link)
         songList.append(songID)
@@ -26,8 +28,6 @@ def getInfo (page):
         artistList.append (artist)
         titleList.append(title)
         
-        x+=1
-
 def getLyrics (url):
     r = requests.get('https://genius.com'+url)
     s = BeautifulSoup(r.text, 'html.parser')
@@ -38,20 +38,24 @@ def getLyrics (url):
 
     released = s.find('span', class_='metadata_unit-info metadata_unit-info--text_only')
     try:
-        releasedList.append(released.text)
+        if any(month in released.text for month in months):
+            releasedList.append(released.text)
+        else:
+            releasedList.append('N/A')  
     except:
-        releasedList.append('')
-    print (url)
+        releasedList.append('N/A')
+    print ('Grabbing lyrics from ' + url)
 
 for i in page:
     getInfo(i)
 
-rankCounter = 0
 for i in linkList:
     getLyrics(i)
-    rankCounter +=1
-    rankList.append(rankCounter)
 
-with open('raw-output.csv','a', newline='', encoding = 'utf-8') as f:
+with open(outputPath+ fileDate + '-genius-top-100.csv','w', newline='', encoding = 'utf-8') as f:
     writer = csv.writer(f)
-    writer.writerows(zip(dateList, songList, rankList, artistList, titleList, viewList, releasedList, linkList, lyricsList))
+    writer.writerows(zip(['Time_Checked'], ['Song_ID'], ['Rank'], ['Artist'], \
+                        ['Title'], ['Views'], ['Date_Published'], ['Link'], ['Lyrics']))
+    writer.writerows(zip(dateList, songList,
+                        (i for i in range(1,101)), 
+                        artistList, titleList, viewList, releasedList, linkList, lyricsList))
