4edb69162e909cdb7858c39d5cc7235d14f53946
Jeff Brennan <jeffbrennan10@gmail.com>
2018-08-26 15:58:58 -0500

    replaced lists with dict+pandas

--- genius-scrape.py
+++ genius-scrape.py
@@ -1,61 +1,61 @@
-import requests, csv, time, timing, json
+import requests, time, timing, json, pandas
 from bs4 import BeautifulSoup
 
-releasedList, linkList, songList, viewList, lyricsList, artistList, titleList, dateList = ([] for i in range(8))
-page = [1,2]
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', \
         'August', 'September', 'October', 'November', 'December']
-outputPath = 'C:/Users/jeffb/Documents/Python/webPrograms/webScraping/genius/output/genius/'
+
+header = ['Song_ID', 'Time_Checked', 'Artist', 'Title', 'Views', 'Link', 'Date_Published', 'Lyrics']
+
+
+outputPath = 'C:/Users/jeffb/Documents/Python/webPrograms/webScraping/genius/output/genius-songs/'
 fileDate = time.strftime('%Y-%m-%d')
+date = time.time()
 
-checked = time.time()
+results = {}
 
-def getInfo (page):
-    r = requests.get('https://genius.com/api/songs/chart?page='+str(page)+'&per_page=50&time_period=day')
-    data = json.loads(r.content.decode())
+def getSongInfo ():
+    for page in range(1, 3):
+        rank = (50 * page) - 49 ## (enables 1-100 rank w/ 50 results per page)
+        r = requests.get('https://genius.com/api/songs/chart?page='+str(page)+'&per_page=50&time_period=day')
+        data = json.loads(r.content.decode())
+        for i in range(0, 50): ## (i=0 -> i=49 | corresponds to list positions for items 1,50)
+            link = data['response']['chart_items'][i]['item']['path']
+            song = data['response']['chart_items'][i]['item']['id']
+            views = data['response']['chart_items'][i]['item']['stats']['pageviews']
+            artist = data['response']['chart_items'][i]['item']['primary_artist']['name']       
+            title = data['response']['chart_items'][i]['item']['title']
 
-    for i in range(0, 50):
-        link = data['response']['chart_items'][i]['item']['path']
-        songID = data['response']['chart_items'][i]['item']['id']
-        views = data['response']['chart_items'][i]['item']['stats']['pageviews']
-        artist = data['response']['chart_items'][i]['item']['primary_artist']['name']       
-        title = data['response']['chart_items'][i]['item']['title']
+            results[rank] = [song, date, artist, title, views, link]
+            rank += 1
 
-        linkList.append(link)
-        songList.append(songID)
-        viewList.append(views)
-        dateList.append(checked)
-        artistList.append (artist)
-        titleList.append(title)
-        
-def getLyrics (url):
+def getLyrics (i):
+    url = results[i][5]
     r = requests.get('https://genius.com'+url)
-    s = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-    lyrics = s.find('div', class_='lyrics')
-    for p in lyrics('p'):
-        lyricsList.append(p.text)
-
-    released = s.find('span', class_='metadata_unit-info metadata_unit-info--text_only')
+    released = soup.find('span', class_='metadata_unit-info metadata_unit-info--text_only')
     try:
         if any(month in released.text for month in months):
-            releasedList.append(released.text)
+            results[i].append(released.text)
         else:
-            releasedList.append('N/A')  
+            results[i].append('N/A')  
     except:
-        releasedList.append('N/A')
-    print ('Grabbing lyrics from ' + url)
+        results[i].append('N/A')
 
-for i in page:
-    getInfo(i)
+    lyrics = soup.find('div', class_='lyrics')
+    
+    for lyric in lyrics('p'):pass
+    if lyric: 
+        results[i].append(lyric.text)
+        
 
-for i in linkList:
+    print ('(' + str(i) + ') Grabbing lyrics from ' + url)
+
+getSongInfo()
+
+for i in results:
     getLyrics(i)
 
-with open(outputPath+ fileDate + '-genius-top-100.csv','w', newline='', encoding = 'utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerows(zip(['Time_Checked'], ['Song_ID'], ['Rank'], ['Artist'], \
-                        ['Title'], ['Views'], ['Date_Published'], ['Link'], ['Lyrics']))
-    writer.writerows(zip(dateList, songList,
-                        (i for i in range(1,101)), 
-                        artistList, titleList, viewList, releasedList, linkList, lyricsList))
+df = pandas.DataFrame(results).T
+df.columns = header
+df.to_csv(outputPath + fileDate + '-genius-top-100.csv')
