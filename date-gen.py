888f64de08f16ac923bfb794cc654d61a9bdf8f4
Jeff Brennan <jeffbrennan10@gmail.com>
2018-10-24 21:20:41 -0500

    update to match flake8

--- date-gen.py
+++ date-gen.py
@@ -1,10 +1,15 @@
-import requests, time, timing, json, pandas, datetime, csv
+import requests
+import time
+import timing
+import pandas
+import datetime
+import csv
 from bs4 import BeautifulSoup
 
 def getSaturdays():
     getdate = datetime.datetime.now()
     getday = datetime.datetime.now().weekday()
-    datefix = (getdate + datetime.timedelta(days=5-getday)).date()
+    datefix = (getdate + datetime.timedelta(days=5 - getday)).date()
 
     startdate = datefix.strftime('%Y-%m-%d')
     enddate = '1961-12-25'
@@ -13,17 +18,16 @@ def getSaturdays():
 
     d2 = datetime.datetime.strptime(startdate, '%Y-%m-%d')
     d1 = datetime.datetime.strptime(enddate, '%Y-%m-%d')
-    delta = d2-d1
-    weekCount = delta.days//7
+    delta = d2 - d1
+    weekCount = delta.days // 7
 
-    with open('datelist.csv','w', newline='') as f:
+    with open('datelist.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         for i in range(1, weekCount):
-            dayint = i*7
+            dayint = i * 7
             week = (currentday - datetime.timedelta(days=dayint))
             writer.writerow([week])
 def getMondays():
-    
     startdate = '1958-08-04'
     enddate = '1961-12-25'
 
@@ -31,14 +35,13 @@ def getMondays():
 
     d1 = datetime.datetime.strptime(startdate, '%Y-%m-%d')
     d2 = datetime.datetime.strptime(enddate, '%Y-%m-%d')
-    delta = d2-d1
-    weekCount = delta.days//7
+    delta = d2 - d1
+    weekCount = delta.days // 7
 
-
-    with open('datelist.csv','a', newline='') as f:
+    with open('datelist.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         for i in range(1, weekCount):
-            dayint = i*7
+            dayint = i * 7
             week = (cleanend - datetime.timedelta(days=dayint))
             writer.writerow([week])
 
