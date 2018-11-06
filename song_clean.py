906f8405c3f49f0a1b181d02536a5ea1dc8180b5
Jeff Brennan <jeffbrennan10@gmail.com>
2018-11-06 10:39:23 -0600

    Combined string comparison + search term cleaning for Genius

---
+++ song_clean.py
@@ -0,0 +1,76 @@
+import difflib
+import timing
+import pandas as pd
+import re
+from itertools import compress
+
+ratiolist = []
+songlist = []
+linklist = []
+search_list = []
+
+def unique_get(df):
+    # Creates combined column (assuming it does not exist and removes duplicate values)
+    # df['Combined'] = df['Artist'] + ' ' + df['Title']
+    df = df.drop_duplicates(subset='Combined')
+    df.to_csv('unique-billboard-songs.csv')
+    return df
+
+def censor_replace(title, censor_dict):
+    for item in dict.keys():
+        if item in title:
+            title = title.replace(item, censor_dict[item])
+
+    return title
+
+def title_cleaning(song, artist):
+
+    # Song title fixes
+    song = song.lower()
+
+    # removes characters between parentheses | removes info after slash
+    # tested no significant performance decrease, decided not to include conditional statements
+    song = re.sub(r'\(.*\)', '', song)
+    song = song.split('/')[0]
+
+    # removes extraneous symbols
+    song = song.replace("'", '').replace('...', '').replace('!', '')
+
+    # converts censored expletives (csv -> dictionary) to the actual word
+    expletives = pd.read_csv('expletives.csv', index_col='censored').to_dict()['uncensored']
+    song = censor_replace(song, expletives)
+
+    # ------------ #
+    # Artist fixes #
+    artist = artist.lower()
+
+    # removes featured artists for cleaner search string
+    features = ['Featuring', 'featuring', '&', ',', 'feat', 'feat.', 'Feat.']
+    if any(substring in artist for substring in features):
+        bool_results = [s in artist for s in features]
+        sub_loc = (list(compress(range(len(bool_results)), bool_results)))
+        sub = features[sub_loc[0]]
+        artist = artist.split(sub, 1)[0]
+
+    # search_list.append(clean_title + ' ' + clean_artist)
+    # return search_list
+
+def str_compare(song, link):
+    ratio = difflib.SequenceMatcher(None, song, link).ratio()
+    ratiolist.append(ratio)
+    print(len(ratiolist))
+
+# for i in range(len(df)):
+#     strcompare(df['song'][i], df['split-link'][i])
+
+# df['ratio'] = ratiolist
+
+# df.to_csv('genius-link-ratio-2.csv', encoding='ISO-8859-1')
+
+df = pd.read_csv('billboard-songs.csv', encoding='ISO-8859-1', low_memory=False)
+df_unique = unique_get(df)
+
+for i in range(len(df_unique)):
+    title_cleaning(df_unique['Title'][i], df_unique['Artist'][i])
+
+# df_unique['Search term'] = search_list
